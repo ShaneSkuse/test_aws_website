0ffa6f3d7ce421ca7a7d291eb0c364ce6154a481
Shane.Skuse <shane.skuse@thudmedia.com>
2018-08-22 18:00:43 +0100

    Added bower

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
+
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.get("/", (req, res) => {
     res.render('index', {title : "Hey", message : "Hello there!"});
 });
