28dce8b450155af89bfd44a4d44000006d019231
Shane.Skuse <shane.skuse@thudmedia.com>
2018-08-22 17:41:30 +0100

    Added basic templates using pug

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send('Hello World'));
+app.set('view engine', 'pug');
+app.get("/", (req, res) => {
+    res.render('index', {title : "Hey", message : "Hello there!"});
+});
+
 
 app.listen(port, () => console.log("Example app listening on port " + port + "!"));
